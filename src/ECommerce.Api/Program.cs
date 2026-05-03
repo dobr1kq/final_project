a8a8d5770d5727db4ff4c3729dbcc3e4b69821b2
dobr1kq <08022005dma@gmail.com>
2026-05-03 12:39:45 +0300

    feat: add Docker support and 10k data seeding

--- src/ECommerce.Api/Program.cs
+++ src/ECommerce.Api/Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    
+    await context.Database.EnsureCreatedAsync();
+    
+    await DbSeeder.SeedAsync(context);
+}
+
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
---
+++ src/ECommerce.Infrastructure/Persistence/DbSeeder.cs
@@ -0,0 +1,27 @@
+using Bogus;
+using ECommerce.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence;
+
+public static class DbSeeder
+{
+    public static async Task SeedAsync(AppDbContext context)
+    {
+        if (await context.Products.AnyAsync()) return;
+
+        var productFaker = new Faker<Product>()
+            .RuleFor(p => p.Id, f => Guid.NewGuid())
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(p => p.Price, f => decimal.Parse(f.Commerce.Price(10, 1000)))
+            .RuleFor(p => p.StockQuantity, f => f.Random.Int(0, 100))
+            .RuleFor(p => p.Category, f => f.Commerce.Categories(1)[0])
+            .RuleFor(p => p.SKU, f => f.Commerce.Ean13());
+
+        var products = productFaker.Generate(10000);
+
+        await context.Products.AddRangeAsync(products);
+        await context.SaveChangesAsync();
+    }
+}
